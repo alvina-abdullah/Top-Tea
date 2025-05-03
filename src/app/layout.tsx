6438a3bc93f72459a2230da0a8f9bdcb075528d0
Alvina Abdullah <alvinaabdullah16@gmail.com>
2025-05-03 14:01:08 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import '../globals.css'
+import './globals.css'
 import AOSWrapper from "../components/AOSWrapper";
 
 const geistSans = localFont({
